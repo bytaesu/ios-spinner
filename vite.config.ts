2d59b6ce25151c6ec7cf47ebe15af88f1d565074
Taesu <bytaesu@gmail.com>
2025-07-07 10:45:50 +0900

    adjust to hosting

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  root: 'src',
+  build: {
+    outDir: '../dist-demo',
+    emptyOutDir: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
